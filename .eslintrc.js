5979104b8eb721a602354e3b4b9e954badaf882f
Al <>
2018-12-17 14:34:49 +0000

    Clear tracker after early additions
    The issue around unrepresentative early results appears to be solvable
    by allowing the unreliable results to be recorded, then discarding them
    before continuing to record the remainder of the set.
    
    Initialised ESLint to help monitor code styling.

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2015
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "space-in-parens": [
+            "error",
+            "never"
+        ],
+        "comma-spacing": [
+            "error",
+            {
+                "before": false,
+                "after": true
+            }
+        ],
+        "array-bracket-spacing": [
+            "error",
+            "never"
+        ],
+        "no-multi-spaces": "error",
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": "off"
+    }
+};
--- timer.js
+++ timer.js
@@ -1,41 +1,41 @@
-'use strict';
+"use strict";
 
 function testLast() {
   let repetitions = 100000;
 
   let testArray = [1, 2, 3, 4];
   let tracker = 0;
-  for (let i=0; i<repetitions; i++) {
+  for (let i = 0; i < repetitions; i++) {
     let startTime = window.performance.now();
     testArray[testArray.length - 1];
     let endTime = window.performance.now();
     tracker += endTime - startTime;
-    if (i % 10000 === 0) { console.log(Math.round(tracker*10)/10); }
+    if (i % 10000 === 0) console.log(Math.round(tracker * 10) / 10);
   }
   console.log(tracker);
 
   testArray = ["a", "fgdnjk", "qwrtyrupbd", "dfg", "xcvbnm"];
   tracker = 0;
-  for (let i=0; i<repetitions; i++) {
+  for (let i = 0; i < repetitions; i++) {
     let startTime = window.performance.now();
     testArray[testArray.length - 1];
     let endTime = window.performance.now();
     tracker += endTime - startTime;
-    if (i % 10000 === 0) { console.log(Math.round(tracker*10)/10); }
+    if (i % 10000 === 0) console.log(Math.round(tracker * 10) / 10);
   }
   console.log(tracker);
 
   testArray = [];
-  for (let i=0; i<1000000; i++) {
+  for (let i = 0; i < 1000000; i++) {
     testArray.push(1);
   }
   tracker = 0;
-  for (let i=0; i<repetitions; i++) {
+  for (let i = 0; i < repetitions; i++) {
     let startTime = window.performance.now();
     testArray[testArray.length - 1];
     let endTime = window.performance.now();
     tracker += endTime - startTime;
-    if (i % 10000 === 0) { console.log(Math.round(tracker*10)/10); }
+    if (i % 10000 === 0) console.log(Math.round(tracker * 10) / 10);
   }
   console.log(tracker);
 }
@@ -46,15 +46,16 @@ function demonstrateBatchDiscrepancies() {
   let testArray = [1, 2, 3, 4];
   let tracker = 0;
   let waypoints = [];
-  for (let i=0; i<repetitions; i++) {
+  for (let i = 0; i < repetitions; i++) {
+    if (i === 20000) tracker = 0;
     let startTime = window.performance.now();
     testArray[testArray.length - 1];
     let endTime = window.performance.now();
     tracker += endTime - startTime;
-    if (i % 10000 === 0) { waypoints.push(Math.round(tracker*10)/10); }
+    if (i % 10000 === 0) waypoints.push(Math.round(tracker * 10) / 10);
   }
   let intervals = [];
-  for (let i=1; i<10; i++) {
+  for (let i = 1; i < 10; i++) {
     intervals.push(waypoints[i] - waypoints[i - 1]);
   }
   console.log(intervals);
@@ -62,15 +63,15 @@ function demonstrateBatchDiscrepancies() {
   testArray = [1, 2, 3, 4];
   tracker = 0;
   waypoints = [];
-  for (let i=0; i<repetitions; i++) {
+  for (let i = 0; i < repetitions; i++) {
     let startTime = window.performance.now();
     testArray[testArray.length - 1];
     let endTime = window.performance.now();
-    if (i > 10000) { tracker += endTime - startTime; }
-    if (i % 10000 === 0) { waypoints.push(Math.round(tracker*10)/10); }
+    if (i > 10000) tracker += endTime - startTime;
+    if (i % 10000 === 0) waypoints.push(Math.round(tracker * 10) / 10);
   }
   intervals = [];
-  for (let i=1; i<10; i++) {
+  for (let i = 1; i < 10; i++) {
     intervals.push(waypoints[i] - waypoints[i - 1]);
   }
   console.log(intervals);
@@ -78,20 +79,20 @@ function demonstrateBatchDiscrepancies() {
   testArray = [1, 2, 3, 4];
   tracker = 0;
   waypoints = [];
-  for (let i=0; i<repetitions; i++) {
+  for (let i = 0; i < repetitions; i++) {
     let startTime = window.performance.now();
     testArray[testArray.length - 1];
     let endTime = window.performance.now();
-    if (i > 50000) { tracker += endTime - startTime; }
-    if (i % 10000 === 0) { waypoints.push(Math.round(tracker*10)/10); }
+    if (i > 50000) tracker += endTime - startTime;
+    if (i % 10000 === 0) waypoints.push(Math.round(tracker * 10) / 10);
   }
   intervals = [];
-  for (let i=1; i<10; i++) {
+  for (let i = 1; i < 10; i++) {
     intervals.push(waypoints[i] - waypoints[i - 1]);
   }
   console.log(intervals);
 }
 
 (function() {
-  document.addEventListener("load", testLast())
+  document.addEventListener("load", testLast());
 })();
