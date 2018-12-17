a3379dc1d96f39428aa7b4dd2b4668d022dd4674
Al <>
2018-12-17 13:20:38 +0000

    Extend basic test
    Added iterations through arrays containing different data type/varying
    size of array entries, and through a very large array.
    
    Although the size of array has no noticeable impact on the times
    recorded by the test, creating an array orders of magnitude larger than
    1,000,000 had a significant effect on browser performance - the page
    would not load when trying to create an array of 100,000,000 entries.
    
    There is a second function which more clearly demonstrates the issue
    previously identified, whereby the first timings added to the total are
    significantly longer than those run later in the test. There was no
    noticeable benefit from adding an eventListener to run the script only
    after the page loads; and by disregarding an initial portion of the
    timings measured, it is clear the lag is not related to when the script
    or function starts running - or at least, not uniquely so - rather it is
    evident for the first 20,000-30,000 timings that are added to the total.

--- timer.js
+++ timer.js
@@ -1,21 +1,97 @@
 'use strict';
 
-(function() {
-  let testArray = [1,2,3,4];
+function testLast() {
+  let repetitions = 100000;
+
+  let testArray = [1, 2, 3, 4];
   let tracker = 0;
-  for(let i=0; i<100000; i++) {
+  for (let i=0; i<repetitions; i++) {
     let startTime = window.performance.now();
     testArray[testArray.length - 1];
     let endTime = window.performance.now();
     tracker += endTime - startTime;
-    if(i % 10000 === 0){ console.log(tracker); }
+    if (i % 10000 === 0) { console.log(Math.round(tracker*10)/10); }
   }
   console.log(tracker);
 
-  // let startTime = window.performance.now();
-  // for(let i=0; i<100000; i++) {
-  //   testArray[testArray.length - 1];
-  // }
-  // let endTime = window.performance.now();
-  // console.log(endTime - startTime);
+  testArray = ["a", "fgdnjk", "qwrtyrupbd", "dfg", "xcvbnm"];
+  tracker = 0;
+  for (let i=0; i<repetitions; i++) {
+    let startTime = window.performance.now();
+    testArray[testArray.length - 1];
+    let endTime = window.performance.now();
+    tracker += endTime - startTime;
+    if (i % 10000 === 0) { console.log(Math.round(tracker*10)/10); }
+  }
+  console.log(tracker);
+
+  testArray = [];
+  for (let i=0; i<1000000; i++) {
+    testArray.push(1);
+  }
+  tracker = 0;
+  for (let i=0; i<repetitions; i++) {
+    let startTime = window.performance.now();
+    testArray[testArray.length - 1];
+    let endTime = window.performance.now();
+    tracker += endTime - startTime;
+    if (i % 10000 === 0) { console.log(Math.round(tracker*10)/10); }
+  }
+  console.log(tracker);
+}
+
+function demonstrateBatchDiscrepancies() {
+  let repetitions = 100000;
+
+  let testArray = [1, 2, 3, 4];
+  let tracker = 0;
+  let waypoints = [];
+  for (let i=0; i<repetitions; i++) {
+    let startTime = window.performance.now();
+    testArray[testArray.length - 1];
+    let endTime = window.performance.now();
+    tracker += endTime - startTime;
+    if (i % 10000 === 0) { waypoints.push(Math.round(tracker*10)/10); }
+  }
+  let intervals = [];
+  for (let i=1; i<10; i++) {
+    intervals.push(waypoints[i] - waypoints[i - 1]);
+  }
+  console.log(intervals);
+
+  testArray = [1, 2, 3, 4];
+  tracker = 0;
+  waypoints = [];
+  for (let i=0; i<repetitions; i++) {
+    let startTime = window.performance.now();
+    testArray[testArray.length - 1];
+    let endTime = window.performance.now();
+    if (i > 10000) { tracker += endTime - startTime; }
+    if (i % 10000 === 0) { waypoints.push(Math.round(tracker*10)/10); }
+  }
+  intervals = [];
+  for (let i=1; i<10; i++) {
+    intervals.push(waypoints[i] - waypoints[i - 1]);
+  }
+  console.log(intervals);
+
+  testArray = [1, 2, 3, 4];
+  tracker = 0;
+  waypoints = [];
+  for (let i=0; i<repetitions; i++) {
+    let startTime = window.performance.now();
+    testArray[testArray.length - 1];
+    let endTime = window.performance.now();
+    if (i > 50000) { tracker += endTime - startTime; }
+    if (i % 10000 === 0) { waypoints.push(Math.round(tracker*10)/10); }
+  }
+  intervals = [];
+  for (let i=1; i<10; i++) {
+    intervals.push(waypoints[i] - waypoints[i - 1]);
+  }
+  console.log(intervals);
+}
+
+(function() {
+  document.addEventListener("load", testLast())
 })();
