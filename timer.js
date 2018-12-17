d7a1581e594bbc812c0c4376e8652e9f053b0cfb
Al <>
2018-12-17 15:21:06 +0000

    Add tests for array.reverse() method
    Copied and amended the code for testing 'last' to separately test
    'reverse'.
    
    Reduced the size of the large array for testing reverse as it took far
    too long to complete for greater lengths.

--- timer.js
+++ timer.js
@@ -3,6 +3,11 @@
 let repetitions = 100000,
   cutoff = 20000;
 
+function runTests() {
+  testLast();
+  testReverse();
+}
+
 function testLast() {
   let testArray = [1, 2, 3, 4];
   let result = loopLastEntry(testArray);
@@ -17,7 +22,7 @@ function testLast() {
   );
 
   testArray = [];
-  for (let i = 0; i < 1000000; i++) {
+  for (let i = 0; i < 10000; i++) {
     testArray.push(1);
   }
   result = loopLastEntry(testArray);
@@ -38,6 +43,41 @@ function loopLastEntry(array) {
   return tracker;
 }
 
+function testReverse() {
+  let testArray = [1, 2, 3, 4];
+  let result = loopReverse(testArray);
+  console.log(
+    `Small array: ${repetitions} operations completed in ${result.toFixed(1)}ms`
+  );
+
+  testArray = ["a", "fgdnjk", "qwrtyrupbd", "dfg", "xcvbnm"];
+  result = loopReverse(testArray);
+  console.log(
+    `Text array: ${repetitions} operations completed in ${result.toFixed(1)}ms`
+  );
+
+  testArray = [];
+  for (let i = 0; i < 10000; i++) {
+    testArray.push(1);
+  }
+  result = loopReverse(testArray);
+  console.log(
+    `Big array: ${repetitions} operations completed in ${result.toFixed(1)}ms`
+  );
+}
+
+function loopReverse(array) {
+  let tracker = 0;
+  for (let i = 0; i < repetitions + cutoff; i++) {
+    if (i === cutoff) tracker = 0;
+    let startTime = window.performance.now();
+    array.reverse();
+    let endTime = window.performance.now();
+    tracker += endTime - startTime;
+  }
+  return tracker;
+}
+
 (function() {
-  document.addEventListener("load", testLast());
+  document.addEventListener("load", runTests());
 })();
