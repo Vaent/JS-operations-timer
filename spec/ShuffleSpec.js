61479729e8efdffa497f1a3828f55ad8d53076d7
Al <>
2018-12-18 17:55:58 +0000

    Add tests for a shuffle(array) function
    The test for values being moved to new positions checks for a range of
    differences between the old and new indices of each value, to ensure the
    array has not simply been shifted/neighbouring positions swapped etc.
    There is a risk that this will occasionally cause the test to fail if
    the shuffle is truly random; to mitigate this, it is only expected that
    one tenth of the differences will be unique. This test should be kept
    under review going forward and ultimately it may be more appropriate to
    only test this aspect manually.

---
+++ spec/ShuffleSpec.js
@@ -0,0 +1,35 @@
+describe ("Shuffle", function() {
+  let testArray;
+
+  beforeEach (function() {
+    testArray = [];
+    for (let i = 0; i < 100; i++) {
+      testArray.push(i);
+    }
+  });
+
+  it ("the shuffled array is still the same length", function() {
+    let referenceArrayLength = testArray.length;
+    shuffle(testArray);
+    expect(testArray.length).toEqual(referenceArrayLength);
+  });
+  it ("the shuffled array contains all original values", function() {
+    let referenceArray = testArray.slice(0);
+    shuffle(testArray);
+    referenceArray.forEach (function(entry) {
+      expect(testArray).toContain(entry);
+    });
+  });
+  it ("the array has in fact been shuffled", function() {
+    let referenceArray = testArray.slice(0);
+    shuffle(testArray);
+    let differences = [];
+    for (let i = 0; i < referenceArray.length; i++) {
+      let difference = testArray.indexOf(referenceArray[i]) - i;
+      if (differences.indexOf(difference) < 0) {
+        differences.push(difference);
+      }
+    };
+    expect(differences.length).toBeGreaterThan(referenceArray.length / 10);
+  });
+});
