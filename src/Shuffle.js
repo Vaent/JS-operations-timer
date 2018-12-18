c6edd7608bf2caea79aee6dd2f9f86638368598b
Al <>
2018-12-18 18:15:32 +0000

    Implement shuffle function which passes all tests
    The function has also been manually tested in the browser console and
    appears to be producing well-shuffled arrays.

--- src/Shuffle.js
+++ src/Shuffle.js
@@ -1,5 +1,9 @@
 "use strict";
 
 function shuffle(array) {
-  
+  let originalArray = array.slice(0);
+  for (let i = 0; i < array.length; i++) {
+    let randomIndex = Math.floor(Math.random() * originalArray.length);
+    array[i] = originalArray.splice(randomIndex, 1)[0];
+  }
 }
