1027258b8f565adccdf8597e8a430a2a7058dfa1
Al <>
2018-12-18 16:32:30 +0000

    Add tests for a function to detect duplicates

---
+++ spec/CheckForADuplicateSpec.js
@@ -0,0 +1,18 @@
+"use strict";
+
+describe ("Check array for a duplicate", function() {
+  it ("returns false if the array contains no duplicates", function() {
+    let testArray = [];
+    for (let i = 0; i < 1000; i++) {
+      testArray.push(i);
+    }
+    expect(containsDuplicate(testArray)).toBe(false);
+  });
+  it ("returns true if the array contains at least one duplicate", function() {
+    let testArray = [];
+    for (let i = 0; i < 1000; i++) {
+      testArray.push(i%998);
+    }
+    expect(containsDuplicate(testArray)).toBe(true);
+  });
+});
