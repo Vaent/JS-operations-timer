ba681f709927e8b3a9fb0490a58b3926e7650c6c
Al <>
2018-12-18 16:41:50 +0000

    Implement checking for duplicate entries

--- src/CheckForADuplicate.js
+++ src/CheckForADuplicate.js
@@ -1,5 +1,10 @@
 "use strict";
 
 function containsDuplicate(array) {
-  
+  for (let i = 0; i < array.length -1; i ++) {
+    for (let j = i + 1; j < array.length; j ++) {
+      if (array[i] === array[j]) { return true; }
+    }
+  }
+  return false;
 }
