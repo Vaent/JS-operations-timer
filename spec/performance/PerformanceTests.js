9a7c66e7cd666bffa10c22c351161e8f6c03e5a9
Al <>
2018-12-18 15:11:33 +0000

    Add performance test for assigning to groups

--- spec/performance/PerformanceTests.js
+++ spec/performance/PerformanceTests.js
@@ -20,11 +20,16 @@ function doUnshiftOneElement(array) {
   array.unshift(0);
 }
 
+function doAssignToGroups(listOfStudents) {
+  assignStudentsToGroups(listOfStudents, 4);
+}
+
 (function() {
 // test(method, label, repetitions, arrayStepSize, arrayNumberOfSteps)
-  test(doLast, "last", 10, 100000, 20);
-  test(doReverse, "reverse", 20, 20000, 20);
-  test(doSort, "sort", 5, 20000, 20);
-  test(doShuffle, "shuffle", 5, 5000, 20);
-  test(doUnshiftOneElement, "unshift", 20, 20000, 20);
+  // test(doLast, "last", 10, 100000, 20);
+  // test(doReverse, "reverse", 20, 20000, 20);
+  // test(doSort, "sort", 5, 20000, 20);
+  // test(doShuffle, "shuffle", 5, 5000, 20);
+  // test(doUnshiftOneElement, "unshift", 20, 20000, 20);
+  test(doAssignToGroups, "assign students to groups", 20, 10000, 20);
 })();
